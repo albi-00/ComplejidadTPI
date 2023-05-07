47f276f8a91cd3215a832c7a3bbd118fd40ad775
Encinas Maria del Alba <albieeeeee17@gmail.com>
2023-05-07 13:36:05 -0300

    commit 2

--- trabajoperfecto.py
+++ trabajoperfecto.py
@@ -1,23 +1,28 @@
-N= int(input ('ingrese N= '))
-K= int(input ('ingrese K= '))
+import time
 
 def calculadivisoresdeN(N:int) -> list:   
     divisoresN=list()
     for j in range(1,N+1): 
         if (N % j)==0: 
             divisoresN.append(int(j))
-    print(divisoresN)
     return divisoresN
-divisores = calculadivisoresdeN(N)
 
-suma=0
-for i in divisores:
-    suma=i+suma
-print(suma)
+def esMulti(suma, N, K):
+        if suma==(K*N):
+            print('es multi')
+        else:
+            print('NO es multi')
 
-def esMulti(suma):
-    if suma==(K*N):
-        print('es multi')
-    else:
-        print('NO es multi')
-esMulti(suma) 
+def main(N,K):
+    tiempoinicio=time.time()
+    divisores = calculadivisoresdeN(N)
+    suma=0
+    for i in divisores:
+        suma=i+suma
+    esMulti(suma,N,K) 
+    tiempototal=time.time()-tiempoinicio
+    print('tiempo es',tiempototal)
+
+N= int(input ('ingrese N= '))
+K= int(input ('ingrese K= '))
+main(N,K)
